da5c41328b18f9be0beae2e5c1b20be3f1ac89c4
ericfeldman1 <ericfeldman2@gmail.com>
2019-10-23 21:01:46 -0400

    working app

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const MOVIEDEX = require('./moviedex.json')
 
 const app = express()
 
+// First middelware - runs on every route
 app.use(morgan('dev'))
 
+// Second middleware - runs on every route
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
   
-    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    if (!authToken || authToken.split(" ")[1] !== apiToken) {
       return res.status(401).json({ error: 'Unauthorized request' })
     }
     // move to the next middleware
@@ -19,7 +21,7 @@ app.use(function validateBearerToken(req, res, next) {
   })
 
 app.get('/movie', function handleGetMovie(req, res) {
-    let response = MOVIEDEX.genre;
+    let response = MOVIEDEX;
   
     // filter our movie by genre if genre query param is present
     if (req.query.genre) {
